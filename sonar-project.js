1cdf9eb04ccdd985f1adafd1116caa6b837942f2
Gitblix <135981516+Gitblix@users.noreply.github.com>
2023-06-29 02:01:55 +0000

    Updated With Sonar URL & Token - Annex

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsJomacsit',
-	    'sonar.login': '98e0a0d0731108b38bfaee59fedac2eb4f612a7a',
+	    'sonar.login': '678a9caa20f106ad09ffac07fb6a81eea20f0a2b',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
