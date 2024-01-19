7fff6a934410f7ed19f9481f631d4afa2d2cb404
Ben [B.T] <135981516+Gitblix@users.noreply.github.com>
2024-01-19 14:43:11 +0000

    Updated with sonar Url

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.27.223:9000/',
+  serverUrl: 'http://172.31.21.255:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsJomacsit',
-	    'sonar.login': '678a9caa20f106ad09ffac07fb6a81eea20f0a2b',
+	    'sonar.login': 'ff08ec6efa35e7aa5528f1ed7fe7cf304882254d',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
